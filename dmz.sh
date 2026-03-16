36e015d07f753acc080cfc3492601b0542558152
Luis Banha <luisbanha26@gmail.com>
2026-03-16 21:59:11 +0000

    fsa

--- dmz.sh
+++ dmz.sh
@@ -2,13 +2,18 @@ sudo ifconfig enp0s8 23.214.219.129 netmask 255.255.255.128
 sudo ifconfig enp0s3 down
 sudo route add default gw 23.214.219.254
 
+IP_DNS="23.214.219.130"
+IP_SMTP="23.214.219.131"
+IP_MAIL="23.214.219.132"
+IP_WWW="23.214.219.133"
+IP_VPN_GW="23.214.219.134"
 
-# Add Web server IP
-sudo ip addr add 23.214.219.133/25 dev enp0s8
+sudo ip addr add $IP_DNS/25 dev enp0s8
+sudo ip addr add $IP_SMTP/25 dev enp0s8
+sudo ip addr add $IP_MAIL/25 dev enp0s8
+sudo ip addr add $IP_WWW/25 dev enp0s8
+sudo ip addr add $IP_VPN_GW/25 dev enp0s8
 
-# Add Mail server IP
-sudo ip addr add 23.214.219.132/25 dev enp0s8
 
-# Add DNS server IP
-sudo ip addr add 23.214.219.130/25 dev enp0s8
+
 
--- internal.sh
+++ internal.sh
@@ -1,3 +1,9 @@
 sudo ifconfig enp0s8 192.168.10.1 netmask 255.255.255.0
 sudo ifconfig enp0s3 down
 sudo route add default gw 192.168.10.254
+
+IP_DATASTORE="192.168.10.2"
+IP_FTP="192.168.10.3"
+
+sudo ip addr add $IP_DATASTORE/24 dev enp0s8
+sudo ip addr add $IP_FTP/24 dev enp0s8
