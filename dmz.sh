345053970c142e72240ae9228a0bccf66f4be9fa
Luis Banha <luisbanha26@gmail.com>
2026-03-16 18:41:16 +0000

    alias

--- dmz.sh
+++ dmz.sh
@@ -1,2 +1,11 @@
 sudo ifconfig enp0s8 23.214.219.129 netmask 255.255.255.128
 
+# Add Web server IP
+sudo ip addr add 23.214.219.133/25 dev enp0s3
+
+# Add Mail server IP
+sudo ip addr add 23.214.219.132/25 dev enp0s3
+
+# Add DNS server IP
+sudo ip addr add 23.214.219.130/25 dev enp0s3
+
