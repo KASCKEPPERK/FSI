7df4c0203a0b3f5459331557abb963ddcb4d69c4
Luis Banha <luisbanha26@gmail.com>
2026-03-16 18:42:37 +0000

    alias fix

--- dmz.sh
+++ dmz.sh
@@ -1,11 +1,11 @@
 sudo ifconfig enp0s8 23.214.219.129 netmask 255.255.255.128
 
 # Add Web server IP
-sudo ip addr add 23.214.219.133/25 dev enp0s3
+sudo ip addr add 23.214.219.133/25 dev enp0s8
 
 # Add Mail server IP
-sudo ip addr add 23.214.219.132/25 dev enp0s3
+sudo ip addr add 23.214.219.132/25 dev enp0s8
 
 # Add DNS server IP
-sudo ip addr add 23.214.219.130/25 dev enp0s3
+sudo ip addr add 23.214.219.130/25 dev enp0s88
 
