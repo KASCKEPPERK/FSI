90a214769ea6c102dabeff5e36a9e359daff356a
Luis Banha <luisbanha26@gmail.com>
2026-03-16 19:08:07 +0000

    default gateway

--- dmz.sh
+++ dmz.sh
@@ -1,4 +1,6 @@
 sudo ifconfig enp0s8 23.214.219.129 netmask 255.255.255.128
+sudo ip route add gw 23.214.219.254
+
 
 # Add Web server IP
 sudo ip addr add 23.214.219.133/25 dev enp0s8
--- internal.sh
+++ internal.sh
@@ -1,1 +1,2 @@
 sudo ifconfig enp0s8 192.168.10.1 netmask 255.255.255.0
+sudo ip route add gw 192.168.10.254
