3fddb74582aa141df1fb2390031f521e176269f3
Cardetas <cardetas612@gmail.com>
2026-04-27 12:03:18 +0100

    idk

--- TP2/vpn_server.sh
+++ TP2/vpn_server.sh
@@ -7,5 +7,7 @@ sudo ifconfig enp0s9 10.60.0.1 netmask 255.255.255.0
 sudo cp CA/ca.crt /etc/pki/CA/ca.crt
 sudo cp CA/vpn.key /etc/pki/CA/vpn.key
 sudo cp CA/vpn.crt /etc/pki/CA/vpn.crt
+sudo cp CA/dh2048.pem /etc/openvpn/server/dh2048.pem
+sudo cp server.conf /etc/openvpn/server/server.conf
 
-sudo openvpn --config server.conf
+sudo openvpn --config /etc/openvpn/server/server.conf
