a7e4f992de104e5a1d544edbb712aaff839424d5
Cardetas <cardetas612@gmail.com>
2026-04-28 16:12:41 +0100

    idk

--- TP2/addusers.sh
+++ TP2/addusers.sh
@@ -7,10 +7,10 @@ setup_user() {
   echo "--------------------------------------"
   echo "Setting up: $USERNAME"
 
-  sudo adduser "$USERNAME" --disabled-password --gecos ""
+  sudo adduser "$USERNAME"
   echo "$USERNAME:$PASSWORD" | sudo chpasswd
 
-  sudo -u "$USERNAME" google-authenticator -t -d -f -w 3 -q
+  sudo -u "$USERNAME" google-authenticator -t -d -f -w 3 -q -r 3 -R 30
 
   SECRET=$(sudo head -n 1 /home/"$USERNAME"/.google_authenticator)
   echo "SUCCESS: User $USERNAME created."
