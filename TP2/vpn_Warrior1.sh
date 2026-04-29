44358ca30246eec97bfb94be2773913dede6bca9
root <root@localhost.localdomain>
2026-04-29 18:05:20 +0100

    seperate scripts for clients

---
+++ TP2/vpn_Warrior1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# configure interfaces
+sudo ifconfig enp0s8 193.136.212.2 netmask 255.255.255.0
+
+sudo cp CA/ca.crt /etc/pki/CA/ca.crt
+sudo cp CA/Warrior1.key /etc/pki/CA/client.key
+sudo cp CA/Warrior1.crt /etc/pki/CA/client.crt
+sudo cp client.conf /etc/openvpn/client/client.conf
+
+sudo openvpn --config /etc/openvpn/client/client.conf
--- TP2/vpn_Warrior2.sh
+++ TP2/vpn_Warrior2.sh
@@ -4,8 +4,8 @@
 sudo ifconfig enp0s8 193.136.212.2 netmask 255.255.255.0
 
 sudo cp CA/ca.crt /etc/pki/CA/ca.crt
-sudo cp CA/client.key /etc/pki/CA/client.key
-sudo cp CA/client.crt /etc/pki/CA/client.crt
+sudo cp CA/Warrior2.key /etc/pki/CA/client.key
+sudo cp CA/Warrior2.crt /etc/pki/CA/client.crt
 sudo cp client.conf /etc/openvpn/client/client.conf
 
 sudo openvpn --config /etc/openvpn/client/client.conf
