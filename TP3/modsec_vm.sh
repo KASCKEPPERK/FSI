5bf6105e47d6b55b536394a0a8ab4061097d528d
root <root@localhost.localdomain>
2026-05-25 04:41:53 -0400

    tcp fixed?

--- TP3/modsec_vm.sh
+++ TP3/modsec_vm.sh
@@ -2,7 +2,7 @@ sudo ip addr flush enp0s8
 sudo ip addr flush enp0s9
 
 sudo ip addr add 192.168.56.30/24 dev enp0s8
-sudo ip addr add 192.168.57.20/24 dev enp0s9
+sudo ip addr add 192.168.57.30/24 dev enp0s9
 
 sudo ip link set enp0s8 up
 sudo ip link set enp0s9 up
