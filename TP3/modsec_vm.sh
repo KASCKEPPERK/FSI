5e41d17ddabd48cd2cc85e42f7ec5961652f9cb2
root <root@localhost.localdomain>
2026-05-25 15:12:27 -0400

    back

--- TP3/modsec_vm.sh
+++ TP3/modsec_vm.sh
@@ -1,11 +1,11 @@
-ip addr flush enp0s8
-ip addr flush enp0s9
+sudo ip addr flush enp0s8
+sudo ip addr flush enp0s9
 
-ip addr add 192.168.56.30/24 dev enp0s8
-ip addr add 192.168.57.30/24 dev enp0s9
+sudo ip addr add 192.168.56.30/24 dev enp0s8
+sudo ip addr add 192.168.57.30/24 dev enp0s9
 
-ip link set enp0s8 up
-ip link set enp0s9 up
+sudo ip link set enp0s8 up
+sudo ip link set enp0s9 up
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
-iptables -t nat -A POSTROUTING \-s 192.168.56.0/24 \-o enp0s9 \-j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 192.168.56.0/24 -o enp0s9 -j MASQUERADE
--- TP3/weakapp_vm.sh
+++ TP3/weakapp_vm.sh
@@ -3,8 +3,6 @@ sudo ip addr flush dev enp0s9
 sudo ip addr add 192.168.57.20/24 dev enp0s9
 sudo ip link set enp0s9 up
 
-
-
 sudo systemctl start docker
 
 sudo docker rm -f juice-shop 2>/dev/null || true
