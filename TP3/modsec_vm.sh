c5e7cfe410223541944986162115d9c212d279dd
root <root@kasckepperd.nome>
2026-05-27 06:18:32 +0100

    working on report

--- TP3/modsec_vm.sh
+++ TP3/modsec_vm.sh
@@ -7,5 +7,9 @@ sudo ip addr add 192.168.57.30/24 dev enp0s9
 sudo ip link set enp0s8 up
 sudo ip link set enp0s9 up
 
+sudo systemctl disable firewalld
+sudo systemctl stop firewalld
+nft flush ruleset
+
 echo 1 > /proc/sys/net/ipv4/ip_forward
 sudo iptables -t nat -A POSTROUTING -s 192.168.56.0/24 -o enp0s9 -j MASQUERADE
